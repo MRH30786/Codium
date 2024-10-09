b412323f4b76868630d793102a98fae5fab3f846
MRH30786 <125992794+MRH30786@users.noreply.github.com>
2024-10-09 01:30:27 +0000

    test3

---
+++ CMakeFiles/Mylife.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Mylife.dir/src/main.cpp.o"
+  "Mylife"
+  "Mylife.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Mylife.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
